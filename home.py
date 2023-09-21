d02bae2fe9a809132acae0748c4e383ef8cf7454
novi1235711 <novi1235711@gmail.com>
2023-09-21 16:50:16 +0900

    1st commit

--- home.py
+++ home.py
@@ -19,29 +19,25 @@ credentials = Credentials.from_service_account_file(
 
 gc = gspread.authorize(credentials)
 
-SP_SHEET_KEY = '1rJhhD8Z92qa5Koa8SY9u4v8GgmnuW4mPnQHUo26mBLE'
-NL_SHEET_KEY = '1Yj38mvHd84fQKZbLUUymrPiYV6336ViA-U7jLciEG6g'
+SP_SHEET_KEY = '1-Jym3yoFFhcEpfiEwwb7i8rXqtLKmbr7011_l5dEqzg'
+NL_SHEET_KEY = '1IhGgM9BWXKxO_z0hQvMTkH6-b9Q0_x8wD68nFkcevpI'
 sh = gc.open_by_key(SP_SHEET_KEY)
 nl = gc.open_by_key(NL_SHEET_KEY)
 HD_NAME = 'holidays'
 NL_NAME = 'namelist'
-A1st_NAME = 'A_1st'
 A2nd_NAME = 'A_2nd'
 hds = sh.worksheet(HD_NAME)
 names = nl.worksheet(NL_NAME)
-A1st = sh.worksheet(A1st_NAME)
 A2nd = sh.worksheet(A2nd_NAME)
 holidayslist = hds.get_all_values()
 namelist = names.get_all_values()
-A1stlist = A1st.get_all_values()
 A2ndlist = A2nd.get_all_values()
 df_names = pd.DataFrame(namelist[1:],columns=namelist[0])
 df_holidays = pd.DataFrame(holidayslist[1:],columns=holidayslist[0])
-df_A1st = pd.DataFrame(A1stlist[1:],columns=A1stlist[0])
 df_A2nd = pd.DataFrame(A2ndlist[1:],columns=A2ndlist[0])
 hds_arr =  holidayslist[1:]
-namelist1 = df_names['1年目名簿']
-namelist2 = df_names['2年目名簿']
+namelist2 = df_names['内科医師']
+timelist2 = df_A2nd['Time']
 dtnow = datetime.datetime.now()
 now = dtnow.astimezone(timezone('Asia/Tokyo')).strftime('%Y/%m/%d %H:%M:%S')
 year = int(dtnow.strftime('%Y'))
@@ -49,141 +45,53 @@ month = int(dtnow.strftime('%m'))+1
 if month == 13:
     year = year+1
     month = 1
-namelist_link = '[研修医名簿](https://docs.google.com/spreadsheets/d/1Yj38mvHd84fQKZbLUUymrPiYV6336ViA-U7jLciEG6g/edit?usp=sharing)'
-Table1_link = '[1年目振り分け表](https://docs.google.com/spreadsheets/d/1Ry_BkXERLYMfQ4_mdOT3XdA-PeB76k6cBHjunToLCoE/edit?usp=sharing)'
-Table2_link = '[2年目振り分け表](https://docs.google.com/spreadsheets/d/1jFIYV1Ey36-5_0iWMwZtB9g7VtJjplXAH50tj4Hu4MI/edit?usp=sharing)'
+namelist_link = '[医師名簿](https://docs.google.com/spreadsheets/d/1IhGgM9BWXKxO_z0hQvMTkH6-b9Q0_x8wD68nFkcevpI/edit#gid=1387675448)'
+Table2_link = '[振り分け表](https://docs.google.com/spreadsheets/d/1R5apCzJIKEetmKlEodTpAT8Z2sNdA5lBZcg-ApgVf14/edit#gid=58770345)'
 
 def page_home():
     st.title('Home')
     """
     ##### 回答フォームは左上のメニューから該当のフォームで回答してください。
     ##### (スマホで回答する方は少し上にスクロールして出てくる＞をタップして)
-    #### ＊くれぐれも、絶対に！他人の名前で回答しないようにお願いします。（上書きされます）
+    #### ＊くれぐれも、他人の名前で回答しないようにお願いします。
     #####
     ##### 日当直決め担当者は以下の要領で行ってください。
     ##### 1．左上のメニューから管理者ページにアクセスし、祝日の登録、名簿の確認を行って下さい。
     ##### 2．下記のリンクから振り分け表にアクセスし、表を完成させてください。
     """
-    st.markdown(Table1_link, unsafe_allow_html=True)
     st.markdown(Table2_link, unsafe_allow_html=True)
     """
     ### ＊回答状況＊
     """
-    st.write(df_A1st)
-    st.write(df_A2nd)
-    
-
-def page_form1():
-    st.title('1年目_日当直回答フォーム')
-    """
-    ###### ＊休日は日直・当直それぞれあるので、回答をお願いします。				
-    ###### ＊内科ローテ：外来研修、午前/午後救急に当直明けを被せないこと。				
-    ###### ＊外科ローテ：当直明けがオペ日の場合、メリット/デメリットは考え方次第です。（チームの予定手術は確認すること）				
-    ###### ＊麻酔科ローテ：振替休日を申請しやすいため休日の日直に割り当てやすいですが、1年目で1か月のみとなっているので、考え方次第です。ICU研修があるならその5日間には当直を被せないこと。				
-    ###### ＊救急科ローテ：新規ローテのため指導医に確認ください。				
-    ###### ＊精神科ローテ：原則当直・当直明けでの早退・欠勤を避ける（土曜：日当直，日曜：日直しか入れないので、精神科ローテ月の日当直回数は月2回まで。
-    """
-    with st.form("my_form"):
-        cal = calendar.Calendar().itermonthdays2(year,month)
-        cal_arr = list(cal)
-        new_cal_arr = []
-        new_hds_arr = []
-        for x in cal_arr:
-            if x[0] == 0:
-                del x
-            else:
-                if x[1] == 0:
-                    new_cal_arr.append(str(year)+"/"+str(month)+"/"+str(x[0])+" (月)")
-                elif x[1] == 1:
-                    new_cal_arr.append(str(year)+"/"+str(month)+"/"+str(x[0])+" (火)")
-                elif x[1] == 2:
-                    new_cal_arr.append(str(year)+"/"+str(month)+"/"+str(x[0])+" (水)")
-                elif x[1] == 3:
-                    new_cal_arr.append(str(year)+"/"+str(month)+"/"+str(x[0])+" (木)")
-                elif x[1] == 4:
-                    new_cal_arr.append(str(year)+"/"+str(month)+"/"+str(x[0])+" (金)")
-                elif x[1] == 5:
-                    new_cal_arr.append(str(year)+"/"+str(month)+"/"+str(x[0])+" (土)")
-                elif x[1] == 6:
-                    new_cal_arr.append(str(year)+"/"+str(month)+"/"+str(x[0])+" (日)")
-        for y in cal_arr:
-            if y[0] == 0:
-                del y
-            else:
-                if y[1] == 5:
-                    new_hds_arr.append(str(year)+"/"+str(month)+"/"+str(y[0])+" (土)")
-                elif y[1] == 6:
-                    new_hds_arr.append(str(year)+"/"+str(month)+"/"+str(y[0])+" (日)")
-                else:
-                    del y
-        for z in hds_arr:
-            new_hds_arr.append(str(z[0]))            
-                
+    st.write("間違いや再回答をした場合は、以下の履歴を参考に回答を削除してください。")
+    st.write("＊トラブル回避のため自動的な上書きはされません。また、1つずつしか削除できないようにしています。")
+    df_A2nd = pd.DataFrame(A2ndlist[1:],columns=A2ndlist[0])
+    with st.form("delete2"):
+        st.write(df_A2nd)
+        id2 = st.selectbox("注意して、削除したい回答の時間を選択してください。",timelist2)
+        deleted = st.form_submit_button("Delete")
+        if deleted:
+            df_A2nd = pd.DataFrame(A2ndlist[1:],columns=A2ndlist[0])
+            df_A2nd = df_A2nd[df_A2nd['Time']!=id2]
+            A2nd.clear()
+            set_with_dataframe(A2nd,df_A2nd,row=1,col=1)
+            st.write("選択したデータを削除しました。")
+            st.write("＊もう一度削除する際は、必ずページを再読み込みしてください。")
+            st.write(df_A2nd)
         
-        yourname = st.selectbox(
-            'あなたの名前を選んでください',
-            namelist1
-            )
-
-        comment = st.text_input(
-            'ご要望等あればご記載ください。'
-        )
-
-        left_col, right_col = st.columns(2)
-
-        with left_col:
-
-            '当直がNGな日を選んでください',
-                
-            NG_list1 = []
-            for x in new_cal_arr:
-                checked1 = st.checkbox(str(x),key="cb1"+str(x))
-                if checked1:
-                    NG_list1.append(str(x))
-
-
-        with right_col:
-            '日直がNGな日を選んでください',
-                
-            NG_list2 = []
-            for y in new_hds_arr:
-                checked2 = st.checkbox(str(y),key="cb2"+str(y))
-                if checked2:
-                    NG_list2.append(str(y))
-
-        # Every form must have a submit button.
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            st.write("Time: ", now)
-            st.write("Name: ", yourname)
-            st.write('Your comment: ', comment)
-            st.write('You selected: ', NG_list1, NG_list2)
-            st.write("上記の内容で提出しました")
-            df_A1st = pd.DataFrame(A1stlist[1:],columns=A1stlist[0])
-            data1 = [[now,yourname,comment,NG_list1,NG_list2]]
-            columns1 = ['Time','Name','Comment','NG1','NG2']
-            nd = pd.DataFrame(data=data1,columns=columns1)
-            df_A1st = df_A1st[df_A1st['Name']!=yourname]
-            df_A1st = pd.concat([df_A1st,nd],axis=0,)
-            set_with_dataframe(A1st,df_A1st,row=1,col=1)
-            st.write(df_A1st)
-    
 
 def page_form2():
-    st.title('2年目_日当直回答フォーム')
+    st.title('日当直回答フォーム')
     """
-    ###### ＊休日は日直・当直それぞれあるので、回答をお願いします。				
-    ###### ＊内科ローテ：外来研修、午前/午後救急に当直明けを被せないこと。				
-    ###### ＊外科ローテ：当直明けがオペ日の場合、メリット/デメリットは考え方次第です。（チームの予定手術は確認すること）				
-    ###### ＊麻酔科ローテ：振替休日を申請しやすいため休日の日直に割り当てやすいですが、1年目で1か月のみとなっているので、考え方次第です。ICU研修があるならその5日間には当直を被せないこと。				
-    ###### ＊救急科ローテ：新規ローテのため指導医に確認ください。				
-    ###### ＊精神科ローテ：原則当直・当直明けでの早退・欠勤を避ける（土曜：日当直，日曜：日直しか入れないので、精神科ローテ月の日当直回数は月2回まで。
+    ###### ＊NGの日を基準に振り分けるシステムですが、どうしても特定の日でないと日当直が難しい場合はその旨をコメントに記載した上で希望日をご入力ください。
+    
     """
     with st.form("my_form"):
         cal = calendar.Calendar().itermonthdays2(year,month)
         cal_arr = list(cal)
         new_cal_arr = []
         new_hds_arr = []
+        new_std_arr = []
         for x in cal_arr:
             if x[0] == 0:
                 del x
@@ -213,8 +121,23 @@ def page_form2():
                 else:
                     del y
         for z in hds_arr:
-            new_hds_arr.append(str(z[0]))            
-                
+            new_hds_arr.append(str(z[0]))
+        for w in cal_arr:
+            if w[0] == 0:
+                del w
+            else:
+                if w[1] == 0:
+                    new_std_arr.append(str(year)+"/"+str(month)+"/"+str(w[0])+" (月)")
+                elif w[1] == 1:
+                    new_std_arr.append(str(year)+"/"+str(month)+"/"+str(w[0])+" (火)")
+                elif w[1] == 2:
+                    new_std_arr.append(str(year)+"/"+str(month)+"/"+str(w[0])+" (水)")
+                elif w[1] == 3:
+                    new_std_arr.append(str(year)+"/"+str(month)+"/"+str(w[0])+" (木)")
+                elif w[1] == 4:
+                    new_std_arr.append(str(year)+"/"+str(month)+"/"+str(w[0])+" (金)")
+                else:
+                    del w
         
         yourname = st.selectbox(
             'あなたの名前を選んでください',
@@ -224,28 +147,15 @@ def page_form2():
         comment = st.text_input(
             'ご要望等あればご記載ください。'
         )
+        
+        NG_list1 = st.multiselect("当直がNGの日を入力ください",new_cal_arr)
+        NG_list2 = st.multiselect("日直がNGの日を入力ください",new_hds_arr)
 
-        left_col, right_col = st.columns(2)
+        NG_list3 = st.multiselect("午前がNGの日を入力ください",new_std_arr)
+        NG_list4 = st.multiselect("午後がNGの日を入力ください",new_std_arr)
 
-        with left_col:
-
-            '当直がNGな日を選んでください',
-                
-            NG_list1 = []
-            for x in new_cal_arr:
-                checked1 = st.checkbox(str(x),key="cb1"+str(x))
-                if checked1:
-                    NG_list1.append(str(x))
-
-
-        with right_col:
-            '日直がNGな日を選んでください',
-                
-            NG_list2 = []
-            for y in new_hds_arr:
-                checked2 = st.checkbox(str(y),key="cb2"+str(y))
-                if checked2:
-                    NG_list2.append(str(y))
+        demandsA2 = st.multiselect("当直の希望日があれば入力ください（参考程度です）",new_cal_arr)
+        demandsB2 = st.multiselect("日直の希望日があれば入力ください（参考程度です）",new_hds_arr)
 
         # Every form must have a submit button.
         submitted = st.form_submit_button("Submit")
@@ -253,13 +163,12 @@ def page_form2():
             st.write("Time: ", now)
             st.write("Name: ", yourname)
             st.write('Your comment: ', comment)
-            st.write('You selected: ', NG_list1, NG_list2)
+            st.write('You selected: ', NG_list1, NG_list2, NG_list3, NG_list4)
             st.write("上記の内容で提出しました")
             df_A2nd = pd.DataFrame(A2ndlist[1:],columns=A2ndlist[0])
-            data2 = [[now,yourname,comment,NG_list1,NG_list2]]
-            columns2 = ['Time','Name','Comment','NG1','NG2']
+            data2 = [[now,yourname,comment,NG_list1,NG_list2,demandsA2,demandsB2,NG_list3,NG_list4]]
+            columns2 = ['Time','Name','Comment','NG1','NG2','OK1','OK2','NG3','NG4']
             nd = pd.DataFrame(data=data2,columns=columns2)
-            df_A2nd = df_A2nd[df_A2nd['Name']!=yourname]
             df_A2nd = pd.concat([df_A2nd,nd],axis=0,)
             set_with_dataframe(A2nd,df_A2nd,row=1,col=1)
             st.write(df_A2nd)
@@ -313,15 +222,13 @@ def page_manager():
 selected_page = st.sidebar.radio(
     "メニュー", 
     ["HOME", 
-     "1年目の回答フォーム", "2年目の回答フォーム", "管理者ページ"
+    "回答フォーム", "管理者ページ"
      ]
     )
 
 if selected_page == "HOME":
     page_home()
-elif selected_page == "1年目の回答フォーム":
-    page_form1()
-elif selected_page == "2年目の回答フォーム":
+elif selected_page == "回答フォーム":
     page_form2()
 elif selected_page == "管理者ページ":
     page_manager()
